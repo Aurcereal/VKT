9c1daa7c2fea28aca4a67179d51ba144e9acb978
Aidan <aidannn@seas.upenn.edu>
2026-02-02 21:31:48 -0500

    Recording Rendering into Command Buffer

--- main.cpp
+++ main.cpp
@@ -75,12 +75,14 @@ private:
     vk::SurfaceFormatKHR swapSurfaceFormat;
     vk::Extent2D swapChainExtent;
 
-    // We own so raii?
+    // We own so raii (unlike swapChainImages)?
     vector<vk::raii::ImageView> swapChainImageViews;
 
     vk::raii::PipelineLayout pipelineLayout = nullptr;
     vk::raii::Pipeline graphicsPipeline = nullptr;
 
+    vk::raii::CommandPool commandPool = nullptr;
+    vk::raii::CommandBuffer commandBuffer = nullptr;
 
 #ifdef NDEBUG // Not Debug, Part of C++ Standard
     const bool enableValidationLayers = false;
@@ -588,6 +590,121 @@ private:
 
     }
 
+    void CreateCommandPool() {
+        vk::CommandPoolCreateInfo poolInfo = {
+            .flags = vk::CommandPoolCreateFlagBits::eResetCommandBuffer, // cmd buffers in pool can be rerecorded individually instead of together
+            .queueFamilyIndex = graphicsIndex // all these cmd buffers are only for graphics
+        };
+        commandPool = vk::raii::CommandPool(device, poolInfo);
+
+    }
+
+    void CreateCommandBuffer() {
+        vk::CommandBufferAllocateInfo allocateInfo = {
+            .commandPool = commandPool,
+            .level = vk::CommandBufferLevel::ePrimary, // submitted to queue directly, not used by other cmd bufs, secondary helpful for reusing command ops from primary
+            .commandBufferCount = 1 // command makes multiple, we want one
+        };
+        commandBuffer = std::move(vk::raii::CommandBuffers(device, allocateInfo).front()); // have to move it from buffers list like uPtr
+    }
+
+    // images can be in different layouts at different times
+    // depending on what we're using the img for
+    // presenting has a diff layout than rendering (for optimization sake)
+    void TransitionImageLayout(
+        uint32_t imageIndex,
+
+        vk::ImageLayout oldLayout,
+        vk::ImageLayout newLayout,
+        vk::AccessFlags2 srcAccessMask,
+        vk::AccessFlags2 dstAccessMask,
+        vk::PipelineStageFlags2 srcStageMask,
+        vk::PipelineStageFlags2 dstStageMask
+    ) {
+        vk::ImageMemoryBarrier2 barrier = {
+            .srcStageMask = srcStageMask,
+            .srcAccessMask = srcAccessMask,
+            .dstStageMask = dstStageMask,
+            .dstAccessMask = dstAccessMask,
+            .oldLayout = oldLayout,
+            .newLayout = newLayout,
+            .srcQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED,
+            .dstQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED,
+            .image = swapChainImages[imageIndex],
+            .subresourceRange = {
+                .aspectMask = vk::ImageAspectFlagBits::eColor,
+                .baseMipLevel = 0, .levelCount = 1, .baseArrayLayer = 0, .layerCount = 1
+            }
+        };
+        vk::DependencyInfo dependencyInfo = {
+            .dependencyFlags = {},
+            .imageMemoryBarrierCount = 1,
+            .pImageMemoryBarriers = &barrier
+        };
+        commandBuffer.pipelineBarrier2(dependencyInfo);
+    }
+
+    void RecordCommandBuffer(uint32_t imageIndex) {
+        // if cmd buffer already was recorded, beginning will reset it.  we can't append once we record a cmd buffer, only reset
+        commandBuffer.begin({}); // could put flags in here
+
+        // Transition to COLOR ATTACHMENT OPTIMAL
+        TransitionImageLayout(
+            imageIndex,
+            vk::ImageLayout::eUndefined, // From any?
+            vk::ImageLayout::eColorAttachmentOptimal, // To this format
+            {}, // What access to wait for?  We don't wanna wait for anything
+            vk::AccessFlagBits2::eColorAttachmentWrite,
+            vk::PipelineStageFlagBits2::eColorAttachmentOutput,
+            vk::PipelineStageFlagBits2::eColorAttachmentOutput);
+
+        vk::ClearColorValue clearColor = vk::ClearColorValue(0.0f, 0.0f, 0.0f, 1.0f);
+        vk::RenderingAttachmentInfo attachmentInfo = {
+            .imageView = swapChainImageViews[imageIndex],
+            .imageLayout = vk::ImageLayout::eColorAttachmentOptimal,
+            .loadOp = vk::AttachmentLoadOp::eClear, // what to do before rendering?
+            .storeOp = vk::AttachmentStoreOp::eStore, // what to do after rendering?
+            .clearValue = clearColor
+        };
+
+        vk::RenderingInfo renderingInfo = {
+            .renderArea = {.offset = {0,0}, .extent = swapChainExtent},
+            .layerCount = 1,
+            .colorAttachmentCount = 1,
+            .pColorAttachments = &attachmentInfo // Which color attachments we rendering to?
+        };
+
+        // START RENDER
+        // All record cmds return void so no error handling til we finished recording
+        commandBuffer.beginRendering(renderingInfo);
+        
+        // graphics or compute?
+        commandBuffer.bindPipeline(vk::PipelineBindPoint::eGraphics, graphicsPipeline);
+
+        // remember in the pipeline we specified viewport and scissor state as dynamic, so we gotta specify them now
+        commandBuffer.setViewport(0, vk::Viewport(0.0f, 0.0f, static_cast<float>(swapChainExtent.width), static_cast<float>(swapChainExtent.height)));
+        commandBuffer.setScissor(0, vk::Rect2D(vk::Offset2D(0, 0), swapChainExtent));
+
+        // VertexCount, InstanceCount, FirstVertex (Vertex Buffer Offset), FirstInstance
+        commandBuffer.draw(3, 1, 0, 0);
+
+        commandBuffer.endRendering();
+        // END RENDER
+
+        // Have to transition image layout to presentable format
+        TransitionImageLayout(
+            imageIndex,
+            vk::ImageLayout::eColorAttachmentOptimal,
+            vk::ImageLayout::ePresentSrcKHR,
+            vk::AccessFlagBits2::eColorAttachmentWrite, // We wanna wait for writing ops
+            {},
+            vk::PipelineStageFlagBits2::eColorAttachmentOutput,
+            vk::PipelineStageFlagBits2::eBottomOfPipe);
+
+        commandBuffer.end();
+
+    }
+
     void InitVulkan() {
         CreateInstance();
         SetupDebugMessenger();
@@ -599,6 +716,9 @@ private:
         CreateImageViews();
 
         CreateGraphicsPipeline();
+
+        CreateCommandPool();
+        CreateCommandBuffer();
     }
 
     void MainLoop() {
