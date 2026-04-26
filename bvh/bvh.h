eef715633bc02dc1d62f6f4c5e4816e45d21fb50
Aidan <aidannn@seas.upenn.edu>
2026-04-26 18:57:58 -0400

    Surface Area Heuristic for BVH Construction
    Debug Triangle Intersection Count On

--- bvh/bvh.h
+++ bvh/bvh.h
@@ -42,10 +42,52 @@ private:
 	Triangle& GetTriangle(uint32_t);
 	void SwapTriangles(uint32_t, uint32_t);
 
+	float EvaluateSAH(const BVHNode*, int splitAxis, float splitPos);
+
 	void UpdateNodeBounds(BVHNode*);
 	void ChooseSplitPlane(const BVHNode*, int* pAxis, float* pPos);
+	float ChooseSplitPlaneSAH(const BVHNode*, int* pAxis, float* pPos); // -> cost
 	void Subdivide(BVHNode*);
 };
+
+float BVHBuilder::EvaluateSAH(const BVHNode* node, int splitAxis, float splitPos) {
+	vec3 leftLowExtent, leftHighExtent, rightLowExtent, rightHighExtent;
+	leftLowExtent = rightLowExtent = vec3(std::numeric_limits<float>::max());
+	leftHighExtent = rightHighExtent = vec3(std::numeric_limits<float>::min());
+
+	uint32_t start = node->childStartIndex; uint32_t end = node->childStartIndex + node->triangleCount;
+	uint32_t leftCount = 0, rightCount = 0;
+	for (uint32_t i = start; i < end; i++) {
+		const Triangle& t = GetTriangle(i);
+
+		vec3* lowExtent, * highExtent;
+		if (t.centroid[splitAxis] < splitPos) {
+			lowExtent = &leftLowExtent;
+			highExtent = &leftHighExtent;
+			++leftCount;
+		} else {
+			lowExtent = &rightLowExtent;
+			highExtent = &rightHighExtent;
+			++rightCount;
+		}
+
+		for (int v = 0; v < 3; v++) {
+			*lowExtent = min(*lowExtent, t.pts[v]);
+			*highExtent = max(*highExtent, t.pts[v]);
+		}
+	}
+
+	float totalCost = 0.0f;
+	if (leftCount > 0) {
+		totalCost += leftCount * boundingBoxArea(leftLowExtent, leftHighExtent);
+	}
+	if (rightCount > 0) {
+		totalCost += rightCount * boundingBoxArea(rightLowExtent, rightHighExtent);
+	}
+	assert(totalCost >= 0);
+
+	return totalCost;
+}
 
 BVHBuilder::Triangle& BVHBuilder::GetTriangle(uint32_t i) {
 	return triangles[bvh->triangleRedirection[i]];
@@ -80,15 +122,61 @@ void BVHBuilder::ChooseSplitPlane(const BVHNode* node, int* pAxis, float* pPos) 
 	*pAxis = currAxis;
 }
 
+float BVHBuilder::ChooseSplitPlaneSAH(const BVHNode* node, int* pAxis, float* pPos) {
+	assert(node->triangleCount > 0);
+
+	int currAxis; float currPos;
+	float currCost = -1.0f;
+
+	const int divisions = 16;
+	for (int a = 0; a < 3; a++) {
+		float len = node->highExtent[a] - node->lowExtent[a];
+		for (int split = 1; split < divisions; split++) {
+			float tryPos = node->lowExtent[a] + len * (1.0f * split) / (1.0f * divisions);
+
+			float newCost = EvaluateSAH(node, a, tryPos);
+			if (currCost < 0.0f || newCost < currCost) {
+				currCost = newCost;
+				currAxis = a; currPos = tryPos;
+			}
+		}
+	}
+
+	assert(currCost > 0.0f);
+
+	*pPos = currPos;
+	*pAxis = currAxis;
+
+	return currCost;
+}
+
+uint leafCount = 0;
+
+#define SAH
+
 void BVHBuilder::Subdivide(BVHNode* node) {
-	// Shouldn't split?
-	if (node->triangleCount <= 2)
+#ifdef SAH
+	// Get current cost
+	float currCost = node->triangleCount * boundingBoxArea(node->lowExtent, node->highExtent);
+
+	// Get split plane & return if it's not worth it
+	int splitAxis;  float splitPos;
+	if (ChooseSplitPlaneSAH(node, &splitAxis, &splitPos) >= currCost) {
+		++leafCount;
 		return;
+	}
+#else
+	// Shouldn't split?
+	if (node->triangleCount <= 2) {
+		++leafCount;
+		return;
+	}
 
 	// Get split plane
 	int splitAxis;
 	float splitPos;
 	ChooseSplitPlane(node, &splitAxis, &splitPos);
+#endif
 
 	int32_t i = node->childStartIndex; int32_t j = node->childStartIndex + node->triangleCount - 1;
 
@@ -149,7 +237,7 @@ void BVHBuilder::UpdateNodeBounds(BVHNode* node) {
 // TODO: reserve for pointers to work
 uPtr<BVH> BVHBuilder::BuildBVH(const std::vector<vec3>& vertices, const std::vector<uint32_t>& indices) {
 	bvh = mkU<BVH>();
-	bvh->bvhNodes.reserve(static_cast<uint32_t>(glm::ceil(glm::log2(static_cast<float>(indices.size())))) * indices.size() / 3);
+	bvh->bvhNodes.reserve(indices.size()); // 2 * leafCount >= nodeCount, leafCount = triangleCount = indices.size()/3
 	
 	// Build Triangles
 	for (int i = 0; i < indices.size(); i += 3) {
@@ -169,6 +257,9 @@ uPtr<BVH> BVHBuilder::BuildBVH(const std::vector<vec3>& vertices, const std::vec
 	root->childStartIndex = 0; root->triangleCount = triangles.size();
 	UpdateNodeBounds(root);
 	Subdivide(root);
+
+	std::cout << "BVH Node Count: " << bvh->bvhNodes.size() << std::endl;
+	std::cout << "Leaf Count: " << leafCount << std::endl;
 
 	return std::move(bvh);
 }
--- helper/math.cpp
+++ helper/math.cpp
@@ -12,3 +12,9 @@ int ceilDiv(int num, int den) {
 float trace(glm::mat2x2 m) {
 	return m[0][0] + m[1][1];
 }
+
+float boundingBoxArea(vec3 lowExtent, vec3 highExtent) {
+	vec3 diff = highExtent - lowExtent;
+
+	return 2 * (diff.x * diff.z + diff.x * diff.y + diff.y * diff.z);
+}
